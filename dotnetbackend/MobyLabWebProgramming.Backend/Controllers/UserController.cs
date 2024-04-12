0a6f35f1aeacaf5278f6b9957a24bb82c278112c
stefanstan2402 <stan.stefan240@gmail.com>
2024-04-12 18:48:41 +0300

    modificari User

--- dotnetbackend/MobyLabWebProgramming.Backend/Controllers/UserController.cs
+++ dotnetbackend/MobyLabWebProgramming.Backend/Controllers/UserController.cs
@@ -15,13 +15,17 @@ namespace MobyLabWebProgramming.Backend.Controllers;
 /// </summary>
 [ApiController] // This attribute specifies for the framework to add functionality to the controller such as binding multipart/form-data.
 [Route("api/[controller]/[action]")] // The Route attribute prefixes the routes/url paths with template provides as a string, the keywords between [] are used to automatically take the controller and method name.
-public class UserController : AuthorizedController // Here we use the AuthorizedController as the base class because it derives ControllerBase and also has useful methods to retrieve user information.
+public class UserController : ControllerBase // Here we use the AuthorizedController as the base class because it derives ControllerBase and also has useful methods to retrieve user information.
 {
     /// <summary>
     /// Inject the required services through the constructor.
     /// </summary>
-    public UserController(IUserService userService) : base(userService) // Also, you may pass constructor parameters to a base class constructor and call as specific constructor from the base class.
+    
+    public IUserService userService { get; set; }
+
+    public UserController(IUserService _userService)// Also, you may pass constructor parameters to a base class constructor and call as specific constructor from the base class.
     {
+        userService = _userService;
     }
 
     /// <summary>
@@ -31,11 +35,7 @@ public class UserController : AuthorizedController // Here we use the Authorized
     [HttpGet("{id:guid}")] // This attribute will make the controller respond to a HTTP GET request on the route /api/User/GetById/<some_guid>.
     public async Task<ActionResult<RequestResponse<UserDTO>>> GetById([FromRoute] Guid id) // The FromRoute attribute will bind the id from the route to this parameter.
     {
-        var currentUser = await GetCurrentUser();
-
-        return currentUser.Result != null ? 
-            this.FromServiceResponse(await UserService.GetUser(id)) : 
-            this.ErrorMessageResult<UserDTO>(currentUser.Error);
+        return this.FromServiceResponse(await userService.GetUser(id));
     }
 
     /// <summary>
@@ -45,7 +45,7 @@ public class UserController : AuthorizedController // Here we use the Authorized
     [HttpGet("mail/{email}")] // This attribute will make the controller respond to a HTTP GET request on the route /api/User/GetById/<some_guid>.
     public async Task<ActionResult<RequestResponse<User>>> GetByEmail([FromRoute] String email) // The FromRoute attribute will bind the id from the route to this parameter.
     {
-        return this.FromServiceResponse(await UserService.GetUserByEmail(email));
+        return this.FromServiceResponse(await userService.GetUserByEmail(email));
     }
 
     /// <summary>
@@ -58,11 +58,8 @@ public class UserController : AuthorizedController // Here we use the Authorized
     public async Task<ActionResult<RequestResponse<PagedResponse<UserDTO>>>> GetPage([FromQuery] PaginationSearchQueryParams pagination) // The FromQuery attribute will bind the parameters matching the names of
                                                                                                                                          // the PaginationSearchQueryParams properties to the object in the method parameter.
     {
-        var currentUser = await GetCurrentUser();
-
-        return currentUser.Result != null ?
-            this.FromServiceResponse(await UserService.GetUsers(pagination)) :
-            this.ErrorMessageResult<PagedResponse<UserDTO>>(currentUser.Error);
+        return
+            this.FromServiceResponse(await userService.GetUsers(pagination));
     }
 
     /// <summary>
@@ -72,12 +69,9 @@ public class UserController : AuthorizedController // Here we use the Authorized
     [HttpPost] // This attribute will make the controller respond to a HTTP POST request on the route /api/User/Add.
     public async Task<ActionResult<RequestResponse>> Add([FromBody] UserAddDTO user)
     {
-        var currentUser = await GetCurrentUser();
         user.Password = PasswordUtils.HashPassword(user.Password);
 
-        return currentUser.Result != null ?
-            this.FromServiceResponse(await UserService.AddUser(user, currentUser.Result)) :
-            this.ErrorMessageResult(currentUser.Error);
+        return this.FromServiceResponse(await userService.AddUser(user));
     }
 
     /// <summary>
@@ -87,14 +81,10 @@ public class UserController : AuthorizedController // Here we use the Authorized
     [HttpPut] // This attribute will make the controller respond to a HTTP PUT request on the route /api/User/Update.
     public async Task<ActionResult<RequestResponse>> Update([FromBody] UserUpdateDTO user) // The FromBody attribute indicates that the parameter is deserialized from the JSON body.
     {
-        var currentUser = await GetCurrentUser();
-
-        return currentUser.Result != null ?
-            this.FromServiceResponse(await UserService.UpdateUser(user with
+        return this.FromServiceResponse(await userService.UpdateUser(user with
             {
                 Password = !string.IsNullOrWhiteSpace(user.Password) ? PasswordUtils.HashPassword(user.Password) : null
-            }, currentUser.Result)) :
-            this.ErrorMessageResult(currentUser.Error);
+            }));
     }
 
     /// <summary>
@@ -105,10 +95,7 @@ public class UserController : AuthorizedController // Here we use the Authorized
     [HttpDelete("{id:guid}")] // This attribute will make the controller respond to a HTTP DELETE request on the route /api/User/Delete/<some_guid>.
     public async Task<ActionResult<RequestResponse>> Delete([FromRoute] Guid id) // The FromRoute attribute will bind the id from the route to this parameter.
     {
-        var currentUser = await GetCurrentUser();
-
-        return currentUser.Result != null ?
-            this.FromServiceResponse(await UserService.DeleteUser(id)) :
-            this.ErrorMessageResult(currentUser.Error);
+        return
+            this.FromServiceResponse(await userService.DeleteUser(id));
     }
 }
